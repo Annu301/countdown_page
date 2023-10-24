8381e7f831d796a9b5c7d06090cbb543564c0cea
Annu pal <148860461+Annu301@users.noreply.github.com>
2023-10-24 15:42:47 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,57 @@
+const endDate = "18 june 2022 11:41 AM";
+
+
+document.getElementById("end-date").innerText = "endDate";
+const inputs = document.querySelectorAll("input");
+
+
+
+
+// const clock = () => {
+
+// }  
+
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now) / 1000;
+    
+    if(diff < 0) return;
+    
+    //convert into days;
+    inputs[0].value = Math.floor(diff /3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+//initial call
+clock()
+
+// 1 day = 24 hrs
+// 1 hr = 60 mins
+// 60 mins = 3600 sec
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
